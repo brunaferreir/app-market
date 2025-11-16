13cd1dc6da849424a0cd112794ac0565fa058ed9
Bruna <bruna.fsilva@aluno.faculdadeimpacta.com.br>
2025-11-16 20:38:54 -0300

    adcionando render no frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000"  // ⬅ trocado do Render para o backend local
+    baseURL: "https://marketsystem.onrender.com"  
 });
 
 api.interceptors.request.use((config) => {
